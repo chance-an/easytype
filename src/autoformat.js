21a1ba21c122fb32dc3c8ad676ca286828df51a9
Changsi-An <changsi@veerwest.com>
2011-09-22 18:11:32 -0400

    New code

--- src/autoformat.js
+++ src/autoformat.js
@@ -8,47 +8,71 @@
 //    http://docs.jquery.com/Plugins/Authoring
 //    var
 
-    var globalCache = {};
+    var globalCache = [];
 
-    (function( $ ){
+    function AutoFormatInfoEntry($elem){
+        this.$element = $elem;
+//        this.template = ns.interpretRule();
+        this.templateFragments = [];
+//        this.buildTemplateFragmentList();
+        this.promptLayer = null;
+        this.inputCache = [];
+        this._pasteMonitorHandler = null;
+        this._cutMonitorHandler = null;
+
+        this.mutex1 = false;
+    }
+
+
+     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    (function( $, $$$ ){
 
       var methods = {
          init : function( options ) {
 
            return this.each(function(){
-             $(window).bind('resize.tooltip', methods.reposition);
+               var $this = $(this);
+               var infoEntry = new $$$($this);
+               globalCache.push(infoEntry);
+
+               var aaa = 1;
            });
 
          },
+
          destroy : function( ) {
 
            return this.each(function(){
-             $(window).unbind('.tooltip');
+               //
            })
 
+         },
+
+         /**
+           *  This method is mainly for debugging purpose in dev
+           */
+         cache: function(){
+             return globalCache;
          }
+
       };
 
-      $.fn.tooltip = function( method ) {
-
+      $.fn.autoformat = function( method ) {
         if ( methods[method] ) {
           return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof method === 'object' || ! method ) {
           return methods.init.apply( this, arguments );
         } else {
-          $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+          $.error( 'Method ' +  method + ' does not exist on jQuery.autoformat' );
         }
-
       };
 
-    })( jQuery );
-
-    function initialize(){
+    })( jQuery, AutoFormatInfoEntry);
 
 
-        alert('ok');
-    }
+    AutoFormatInfoEntry.prototype.interpretRule = function(pattern){
 
-    $(document).ready(initialize);
+    };
+
 
 })();
